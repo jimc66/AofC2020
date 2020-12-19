f935ea49d0d18ce53ed30e0b01475f0ce181c578
jimc66 <jim@jimcuff.com>
2020-12-18 21:38:55 -0500

    working day1

--- day12.py
+++ day12.py
@@ -23,16 +23,59 @@ West 270
 challenge number 2
 
 """
-import copy
+#import copy
 # "Globals"
 #FILE_NAME = "day12_input.txt"
 FILE_NAME = "testinput_12.txt"
-CURRENT_DIR = 0
+CURRENT_DIR = 90 #0-North 90-East 180-South 270-West
 NORTH_SOUTH = 0
 EAST_WEST = 0
 
 
-
+def navigate(item):
+    """
+    navigate
+    takes a list [0] char [1]int
+    navigate based on the item
+    modifies the globals
+    CURRENT_DIR
+    NORTH_SOUTH
+    EAST_WEST
+    """
+    global CURRENT_DIR #0-North 90-East 180-South 270-West
+    global NORTH_SOUTH 
+    global EAST_WEST 
+    if item[0] == 'S':
+        NORTH_SOUTH -= item[1]
+    elif item[0] == 'N':
+        NORTH_SOUTH += item[1]
+    elif item[0] == 'E':
+        EAST_WEST += item[1]
+    elif item[0] == 'W':
+        EAST_WEST -= item[1]
+    elif item[0] == 'R':
+        CURRENT_DIR += item[1]
+    elif item[0] == 'L':
+        CURRENT_DIR -= item[1]        
+    # forward and reverse
+    elif item[0] == 'F' or item[0] == 'R':
+        if item[0] == 'R':
+            direction = -item[1] # see if this works or if its too clever
+        else:
+            direction = item[1]
+        if CURRENT_DIR == 0:
+            NORTH_SOUTH += direction
+        elif CURRENT_DIR == 180:
+            NORTH_SOUTH -= direction
+        elif CURRENT_DIR == 90:
+            EAST_WEST += direction
+        elif CURRENT_DIR == 270:
+            EAST_WEST -= direction
+        if CURRENT_DIR > 360:
+            CURRENT_DIR -= 360
+        elif CURRENT_DIR < 0:
+            CURRENT_DIR += 360
+    return
 
 def parselines(all_lines):
     """
@@ -67,7 +110,15 @@ def main():
 #    master_seat_list = []
 ##    new_seat_list = []
     master_dir_list = parselines(all_lines)
-    msg = 'msg : ' 
+#    an_item = []
+    for an_item in master_dir_list:
+        navigate(an_item)
+    msg = "North South: " + str(NORTH_SOUTH)
+    print(msg)
+    msg = "East West: " + str(EAST_WEST)
+    print(msg)
+    man_dist = abs(NORTH_SOUTH) + abs(EAST_WEST)
+    msg = 'Manhattan Distance: ' + str(man_dist)
     print(msg)
 #    msg = 'Final seatlist'
 #    print(msg)
